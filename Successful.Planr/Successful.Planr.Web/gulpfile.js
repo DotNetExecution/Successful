a538c65fd7163f9f39643457c82d23441e0eedd2
Stefan Karlsson <stefan@successful.se>
2015-12-20 23:17:23 +0100

    Added a lot of npm packages, this is because i wan't to remember most of the stuff i am going to use. Started on structuring my gulp work flow.

---
+++ Successful.Planr/Successful.Planr.Web/gulpfile.js
@@ -0,0 +1,23 @@
+﻿'use strict';
+
+var gulp = require('gulp');
+
+var plugins = require('gulp-load-plugins')({
+    DEBUG: false,
+    rename: {
+        'gulp-minify-css': 'minify',
+        'gulp-bower-files': 'bowerFiles'
+    }
+});
+
+// Adding non gulp tasks.
+plugins.del = require('del');                                // Lets you delete files.
+
+// Loads all the tasks.
+var taskLoader = require('gulp-simple-task-loader')({
+    taskDirectory: 'Build/GulpTasks',                        // The directory your tasks are stored in (relative and absolute paths accepted) 
+    configFile: '../Configs/task-loader-config.js',          // The relative path to your task configuration file from your task directory
+    filenameDelimiter: '-',                                  // A character or string of characters to replace in task filenames 
+    tasknameDelimiter: ':',                                  // A character or string of characters to insert in place of removed filenameDelimiter 
+    plugins: plugins                                         // Populates the loader with the plug-ins.
+});
